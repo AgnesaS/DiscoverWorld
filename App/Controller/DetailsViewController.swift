870b0fde2d37067ad191ff7fbbf2fe2df6845fe2
AgnesaS <agnesaselimi8@gmail.com>
2023-04-05 20:32:21 +0200

    Updated

---
+++ App/Controller/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  App
+//
+//  Created by Agnesa  on 16.3.23.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ App/Extension/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  App
+//
+//  Created by Agnesa  on 13.3.23.
+//
+
+import Foundation
